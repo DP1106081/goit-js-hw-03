f0a80eb70fdca5dc122e0e70f020a4452ff91686
DP <dpkds161@gmail.com>
2026-03-26 13:27:02 +0200

    hw3

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,15 @@
-function checkForSpam(message) {
-  const i_1 = 'spam';
-  const i_2 = 'sale';
-  const normal = message.toLowerCase();
-  if (normal.includes(i_1) || normal.includes(i_2)) {
-    return true;
-  } else {
-    return false;
+function filterArray(numbers, value) {
+  let totalNum = [];
+
+  for (const item of numbers) {
+    if (item > value) {
+      totalNum.push(item);
+    }
   }
+  return totalNum;
 }
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
